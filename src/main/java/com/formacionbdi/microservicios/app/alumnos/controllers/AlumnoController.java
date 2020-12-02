82d9951fa90feb6914a2051ca16073d9113314dd
David Domínguez López <daviddominguezlopez@gmail.com>
2020-12-02 10:32:11 +0100

    Versión 0.5

--- src/main/java/com/formacionbdi/microservicios/app/alumnos/controllers/AlumnoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/alumnos/controllers/AlumnoController.java
@@ -11,6 +11,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/formacionbdi/microservicios/app/alumnos/services/AlumnoServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/alumnos/services/AlumnoServiceImpl.java
@@ -2,15 +2,20 @@ package com.formacionbdi.microservicios.app.alumnos.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formacionbdi.microservicios.app.alumnos.client.CursoFeignClient;
 import com.formacionbdi.microservicios.app.alumnos.models.repository.AlumnoRepository;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
 
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
+	
+	@Autowired
+	CursoFeignClient cursoFeignClient;
 
 	@Override
 	@Transactional(readOnly = true)
@@ -22,5 +27,17 @@ public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepositor
 	@Transactional(readOnly = true)
 	public Iterable<Alumno> findAllById(Iterable<Long> ids) {
 		return repository.findAllById(ids);
+	}
+
+	@Override
+	public void eliminarCursoAlumnoPorId(Long id) {
+		cursoFeignClient.eliminarCursoAlumnoPorId(id);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		super.deleteById(id);
+		this.eliminarCursoAlumnoPorId(id);
 	}
 }
